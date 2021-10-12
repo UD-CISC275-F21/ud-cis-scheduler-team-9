4ec1da8679806e9cd737b959d21ee0a87bd5b632
PatrickB <patrick16brady@gmail.com>
2021-10-12 12:30:44 -0400

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App(): JSX.Element {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                UD CIS Scheduler
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        UD CIS Scheduler
+        <p>Patrick Brady</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
