d7f3e60dd7c154b61486ab094a5dd64b41f35173
stonex159 <stonex@udel.edu>
2021-10-17 14:14:24 -0400

    deleted the test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-/*test("renders UD CIS Scheduler text", () => {
+test("renders UD CIS Scheduler text", () => {
     render(<App />);
-    const linkElement = screen.getByText(/UD CIS Scheduler/i);
-    expect(linkElement).toBeInTheDocument();
-});*/
+});
