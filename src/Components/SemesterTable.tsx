8dab52ad02c0ad08c176a8730ebc55501b75c8f2
stonex159 <stonex@udel.edu>
2021-10-18 01:43:56 -0400

    added teh season and year data to the table

--- src/Components/SemesterTable.tsx
+++ src/Components/SemesterTable.tsx
@@ -5,10 +5,25 @@ import { Semester } from "../interface/semester";
 
 export function SemesterTable({semester}: {semester: (Semester)}): JSX.Element {
 
+    function getSeason(){
+        switch(semester.season) {
+        case 0:
+            return "Fall";
+        case 1:
+            return "Winter";
+        case 2:
+            return "Spring";
+        case 3:
+            return "Summer";
+        }
+    }
+
     function renderList(course: Course, index: number){
         return (
             <tr key={index}>
                 <td id="course-name">{course.department+course.courseID}</td>
+                <td id="semester-season">{getSeason()}</td>
+                <td id="semester-year">{semester.year}</td>
             </tr>
         );
     }
@@ -19,6 +34,8 @@ export function SemesterTable({semester}: {semester: (Semester)}): JSX.Element {
                 <thead className="thead-dark">
                     <tr>
                         <th scope="col">Course</th>
+                        <th scope="col">Season</th>
+                        <th scope="col">Year</th>
                     </tr>
                 </thead>
                 <tbody>
