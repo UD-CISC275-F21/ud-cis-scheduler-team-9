8f6634651ad2a834026eb3f215a60c2031c3278c
stonex159 <stonex@udel.edu>
2021-10-17 21:05:46 -0400

    changed from CRLF to LF

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App(): JSX.Element {
     return (
         <Container className="App">
             <Row>
-
+                
             </Row>
             <Row>
                 <ControlPanel showModal={setVisible}></ControlPanel>
