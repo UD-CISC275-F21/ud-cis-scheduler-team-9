21dd34f625c462a122f5264324ae7b69e6ecb094
stonex159 <stonex@udel.edu>
2021-11-03 22:48:23 -0400

    Added a quicksort method to sort plan sequentially

--- src/Components/PlanTable.tsx
+++ src/Components/PlanTable.tsx
@@ -8,23 +8,102 @@ export function PlanTable({ semesters, deleteSemester }: {
     deleteSemester: (deleteIndex: number) => void;
 }): JSX.Element {
 
-    /*function sortPlan(){
+    const arr = planSort();
+
+    function planSort(){
         let i;
         const today = new Date();
-        const newArr = [];
+        const arr1 = [];
+        const arr2 = [];
+        const arr3 = [];
+        const arr4 = [];
+        const newArr: Semester[] = [];
         for(i = 0; i<semesters.length; i++){
             if(semesters[i].year === today.getFullYear()){
-                if(semesters[i].season === 0)
-                    newArr.push(semesters[i]);
-                if(semesters[i].season === 1)
-                    newArr.push(semesters[i]);
-                if(semesters[i].season === 2)
-                    newArr.push(semesters[i]);
-                if(semesters[i].season === 3)
-                    newArr.push(semesters[i]);
+                arr1.push(semesters[i]);
+            } else if(semesters[i].year === today.getFullYear() + 1){
+                arr2.push(semesters[i]);
+            } else if(semesters[i].year === today.getFullYear() + 2){
+                arr3.push(semesters[i]);
+            } else if(semesters[i].year === today.getFullYear() + 3){
+                arr4.push(semesters[i]);
             }
         }
-    }*/
+
+        for(i = 0; i<4; i++){
+            switch(i){
+            case 0:
+                if(arr1){
+                    quickSort(arr1, 0, arr1.length);
+                    newArr.push(...arr1);
+                }
+                break;
+            case 1: 
+                if(arr2){
+                    quickSort(arr2, 0, arr1.length);
+                    newArr.push(...arr2);
+                }
+                break;
+            case 2:
+                if(arr3){ 
+                    quickSort(arr3, 0, arr1.length);
+                    newArr.push(...arr3);
+                }
+                break;
+            case 4:
+                if(arr4){ 
+                    quickSort(arr4, 0, arr1.length);
+                    newArr.push(...arr4);
+                }
+                break;
+
+            }
+        }
+
+        return newArr;
+    }
+
+    function quickSort(array: Array<Semester>, left: 0, right: number = array.length -1) {
+        let index;
+      
+        if (array.length > 1) {
+            index = partition(array, left, right);
+      
+            if (left < index - 1) {
+                quickSort(array, left, index - 1);
+            }
+      
+            if (index < right) {
+                quickSort(array, index, right);
+            }
+        }
+      
+        return array;
+    }
+
+    function partition(array: Array<Semester>, left: 0, right: number = array.length -1) {
+        const pivot = array[Math.floor((left + right) / 2)].season;
+        let i = left;
+        let j = right;
+    
+        while (i <= j) {
+            while (array[i].season < pivot) {
+                i++;
+            }
+        
+            while (array[j].season > pivot) {
+                j--;
+            }
+        
+            if (i <= j) {
+                [array[i].season, array[j].season] = [array[j].season, array[i].season];
+                i++;
+                j--;
+            }
+        }
+    
+        return i;
+    }   
 
     function renderList(the_semester: Semester) {
         return (
@@ -34,7 +113,7 @@ export function PlanTable({ semesters, deleteSemester }: {
 
     return (
         <div className="plan-table" id="plan-table">
-            {semesters.map(renderList)}
+            {arr.map(renderList)}
         </div>
     );
 }
