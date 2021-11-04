a10a0dc5708f95da885d041d7157feb1615feaae
stonex159 <stonex@udel.edu>
2021-11-04 15:09:06 -0400

    deleted forgotten code

--- src/Components/PlanTable.tsx
+++ src/Components/PlanTable.tsx
@@ -8,24 +8,6 @@ export function PlanTable({ semesters, deleteSemester }: {
     deleteSemester: (deleteIndex: number) => void;
 }): JSX.Element {
 
-    /*function sortPlan(){
-        let i;
-        const today = new Date();
-        const newArr = [];
-        for(i = 0; i<semesters.length; i++){
-            if(semesters[i].year === today.getFullYear()){
-                if(semesters[i].season === 0)
-                    newArr.push(semesters[i]);
-                if(semesters[i].season === 1)
-                    newArr.push(semesters[i]);
-                if(semesters[i].season === 2)
-                    newArr.push(semesters[i]);
-                if(semesters[i].season === 3)
-                    newArr.push(semesters[i]);
-            }
-        }
-    }*/
-
     function renderList(the_semester: Semester) {
         return (
             <SemesterCard semester={the_semester} deleteSemester = {deleteSemester}></SemesterCard>
