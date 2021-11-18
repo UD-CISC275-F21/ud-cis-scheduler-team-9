708bc2e6802dcda350c48c5b318d0d6d38772804
stonex159 <stonex@udel.edu>
2021-11-18 15:17:28 -0500

    added a fragment to allow for main merge

--- src/Components/CourseCard.tsx
+++ src/Components/CourseCard.tsx
@@ -5,7 +5,7 @@ import { Button, Card } from "react-bootstrap";
 import { Season } from "../interface/semester";
 
 
-export function CourseCard({cardInfo, setDeleteCard, showCard, hide, hideButton}: {
+export function CourseCard({cardInfo, setDeleteCard, showCard, showPreWarning, hide, hideButton}: {
     cardInfo: Course,
     setDeleteCard:(c:Course) => void, 
     showCard: boolean, 
@@ -71,6 +71,14 @@ export function CourseCard({cardInfo, setDeleteCard, showCard, hide, hideButton}
     return (
         <Card draggable className = "draggable-card" ref={drag} style={{opacity: isDragging ? "50%" : "100%"}}>
             <Card.Body>
+                {showPreWarning && <div className="alert alert-danger d-flex align-items-center" role="alert">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </svg>
+                    <div style = {{ paddingLeft: 10 }}>
+                        You cannot add this course to your semester as it&apos;s prerequisite(s) has not been fufilled in a previous semester ({displayReqs(preReqs)})
+                    </div>
+                </div>}
                 <Card.Title>{cardInfo.department}{cardInfo.courseID}: {cardInfo.title} {!hideButton && <Button className="Danger" onClick={()=>setDeleteCard(cardInfo)}>-</Button>}
                     <Card.Text> Credits: {cardInfo.credits}</Card.Text>
                 </Card.Title> 
