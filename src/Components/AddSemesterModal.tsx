2086f91bd398436fc7e0d63914cd924e07808aca
Izahe <izahe@udel.edu>
2021-12-02 17:19:19 -0500

    commenting out checksemester

--- src/Components/AddSemesterModal.tsx
+++ src/Components/AddSemesterModal.tsx
@@ -5,9 +5,10 @@ import { Course } from "../interface/course";
 import ModalHeader from "react-bootstrap/ModalHeader";
 import { SemesterTable } from "./SemesterTable";
 
-export function AddSemesterModal({ addSemester, checkSemester, setVisible, checkCourse, visible, catalog}:{
+/*Commented out the instances of checkSemester calls until we know how we want to handle it ()*/
+export function AddSemesterModal({ addSemester, /*checkSemester,*/ setVisible, checkCourse, visible, catalog}:{
     addSemester: (s: Semester)=>void,
-    checkSemester: (c: Semester)=>number,
+    /*checkSemester: (c: Semester)=>number,*/
     setVisible: (v:boolean)=>void,
     checkCourse: (c: string)=>boolean,
     visible: (boolean),
