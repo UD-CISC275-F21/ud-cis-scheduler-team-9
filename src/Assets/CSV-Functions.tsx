647d4bec79aac9368a4a4f589cfdd383807d8047
stonex159 <stonex@udel.edu>
2021-12-08 13:00:26 -0500

    attempting to find the stray comma

--- src/Assets/CSV-Functions.tsx
+++ src/Assets/CSV-Functions.tsx
@@ -118,21 +118,18 @@ function extractAsCSV(rows: Semester[]): string{
     }
 
     function produceSemesterData(){
-        // Create our number formatter.
+
         const formatter = new Intl.NumberFormat(undefined, {
             style: "currency",
             currency: "USD",
-        
-            // These options are needed to round to whole numbers if that's what you want.
-            //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-            //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
             //obtained from https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-strings
         });
+
         const semesterData = semesterHeaders.join(",") + "\n" + rows.map(row => {
             return row.year + "," + determineSeason(row.season) + "," + row.creditTotal + "," + `$${formatter.format(row.expectedTuition)}` + "\n";
         });
         console.log(semesterData);
-        
+
         return semesterData;
         
     }
